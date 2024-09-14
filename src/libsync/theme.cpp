d303d6b2fe349039c90b2edd2ed261383dca858c
Xinyang Li <lixinyang411@gmail.com>
2024-09-14 13:17:32 +0800

    Update theme.cpp

--- src/libsync/theme.cpp
+++ src/libsync/theme.cpp
@@ -537,7 +537,7 @@ QString Theme::quotaBaseFolder() const
 
 QString Theme::oauthClientId() const
 {
-    return QStringLiteral("xdXOt13JKxym1B1QcEncf2XDkLAexMBFwiT9j6EfhhHFJhs2KM9jbjTmf8JBXE69");
+    return QStringLiteral("owncloud-desktop");
 }
 
 QString Theme::oauthClientSecret() const
